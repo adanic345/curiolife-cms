999433ca90b701fa87aae6ffd2585163b1a73484
na-p2p <nick.adams@engagep2p.com>
2026-04-29 09:47:00 -0500

    adjust media for other ocntent types

--- src/api/prayer/controllers/prayer.js
+++ src/api/prayer/controllers/prayer.js
@@ -1,9 +1,33 @@
 'use strict';
 
-/**
- * prayer controller
- */
-
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::prayer.prayer');
+module.exports = createCoreController('api::prayer.prayer', () => ({
+  async find(ctx) {
+    const defaultPopulate = { featuredImage: true, externalMedia: true };
+
+    ctx.query = {
+      ...ctx.query,
+      populate:
+        typeof ctx.query.populate === 'object'
+          ? { ...defaultPopulate, ...ctx.query.populate }
+          : defaultPopulate,
+    };
+
+    return await super.find(ctx);
+  },
+
+  async findOne(ctx) {
+    const defaultPopulate = { featuredImage: true, externalMedia: true };
+
+    ctx.query = {
+      ...ctx.query,
+      populate:
+        typeof ctx.query.populate === 'object'
+          ? { ...defaultPopulate, ...ctx.query.populate }
+          : defaultPopulate,
+    };
+
+    return await super.findOne(ctx);
+  },
+}));
